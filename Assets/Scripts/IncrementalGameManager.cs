14bfb835a30c98baba8f69aa31b587670ecaf001
Daniel Medina Medina <motokojunior@gmail.com>
2021-06-02 11:12:50 +0100

    Updated the Repository with the Unity Project

---
+++ Assets/Scripts/IncrementalGameManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IncrementalGameManager : MonoBehaviour
+{
+
+    //Variables
+    public Text cultivationText;
+    public float cultivation_lvl;
+    public float martial_dao_knowledge = 1;
+    public float saint_qi_natural_gain = 0.01f;
+
+
+    void Start()
+    {
+
+        cultivation_lvl = 1;
+        ActionsPerTick();
+    }
+    void Update()
+    {
+        cultivationText.text = string.Format("Cultivation Level: {0:0.00}" , cultivation_lvl);
+        
+    }
+
+
+    public void ActionsPerTick()
+    {
+        InvokeRepeating("SaintQiNaturalGain", 1.0f, 1.0f);
+    }
+    public void Training()
+    {
+        cultivation_lvl = cultivation_lvl + martial_dao_knowledge;
+    }
+
+    public void SaintQiNaturalGain()
+    {
+        
+        cultivation_lvl = cultivation_lvl + saint_qi_natural_gain;
+    }
+}
