29c7967c3eb8b7fc8a1f49d7324d0f146834c600
revileino <revi.leino@hotmail.de>
2021-06-02 17:07:32 +0200

    Mostly clean-up and some testing around

--- Assets/Scripts/IncrementalGameManager.cs
+++ Assets/Scripts/IncrementalGameManager.cs
@@ -5,29 +5,30 @@ public class IncrementalGameManager : MonoBehaviour
 {
 
     //Variables
-    public Text cultivationText;
-    public float cultivation_lvl;
-    public float martial_dao_knowledge = 1;
-    public float saint_qi_natural_gain = 0.01f;
+    [Header("Header")]
+    [SerializeField] private Text cultivation_realm;
+    [SerializeField] private Text cultivation_level_text;
+    [SerializeField] private float cultivation_lvl;
+    [SerializeField] private float martial_dao_knowledge = 1;
+    [SerializeField] private float saint_qi_natural_gain = 0.01f;
 
 
     void Start()
     {
-
         cultivation_lvl = 1;
         ActionsPerTick();
     }
+
     void Update()
     {
-        cultivationText.text = string.Format("Cultivation Level: {0:0.00}" , cultivation_lvl);
-        
+        cultivation_level_text.text = string.Format("Cultivation Level:\n{0:0.00}", cultivation_lvl);
     }
-
 
     public void ActionsPerTick()
     {
         InvokeRepeating("SaintQiNaturalGain", 1.0f, 1.0f);
     }
+
     public void Training()
     {
         cultivation_lvl = cultivation_lvl + martial_dao_knowledge;
@@ -35,7 +36,17 @@ public class IncrementalGameManager : MonoBehaviour
 
     public void SaintQiNaturalGain()
     {
-        
         cultivation_lvl = cultivation_lvl + saint_qi_natural_gain;
+    }
+
+    public void CultivationRealmUpdate(int cultivation) //called in ActionsPerTick() to see if the next realm has been reached
+    {
+        switch (cultivation)
+        {
+            case 10:
+                //level up
+            break;
+        }
+        //return realm;
     }
 }
