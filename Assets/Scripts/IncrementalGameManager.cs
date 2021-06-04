8bff09d971fc986297dd60547c490aa0c05d82fc
revileino <revi.leino@hotmail.de>
2021-06-04 14:51:21 +0200

    Added base for Cultivation Realms

--- Assets/Scripts/IncrementalGameManager.cs
+++ Assets/Scripts/IncrementalGameManager.cs
@@ -1,27 +1,35 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
 
 public class IncrementalGameManager : MonoBehaviour
 {
 
     //Variables
     [Header("Header")]
-    [SerializeField] private Text cultivation_realm;
+    [SerializeField] private Text cultivation_realm_text;
     [SerializeField] private Text cultivation_level_text;
+    [SerializeField] private int cultivation_realm = 0;
     [SerializeField] private float cultivation_lvl;
     [SerializeField] private float martial_dao_knowledge = 1;
     [SerializeField] private float saint_qi_natural_gain = 0.01f;
+
+    Dictionary<string, float> cultivation_realm_dictionary = new Dictionary<string, float>();
 
 
     void Start()
     {
         cultivation_lvl = 1;
+
+        CallRealmDictionary();
         ActionsPerTick();
     }
 
     void Update()
     {
         cultivation_level_text.text = string.Format("Cultivation Level:\n{0:0.00}", cultivation_lvl);
+        cultivation_realm = CultivationRealmUpdate(cultivation_lvl, cultivation_realm);
     }
 
     public void ActionsPerTick()
@@ -39,14 +47,46 @@ public class IncrementalGameManager : MonoBehaviour
         cultivation_lvl = cultivation_lvl + saint_qi_natural_gain;
     }
 
-    public void CultivationRealmUpdate(int cultivation) //called in ActionsPerTick() to see if the next realm has been reached
+    public void CallRealmDictionary()
     {
-        switch (cultivation)
+        string[] cultivation_realm_names_list = {
+            "Qi Condensation stage I",
+            "Qi Condensation stage II",
+            "Qi Condensation stage III",
+            "Qi Condensation stage IV",
+            "Qi Condensation stage V",
+            "Qi Condensation stage VI",
+            "Qi Condensation stage VII",
+            "Qi Condensation stage VIII",
+            "Qi Condensation stage IX",
+        };
+
+        float[] cultivation_realm_exp_threshold_list = {
+            10.00f,
+            20.00f,
+            30.00f,
+            40.00f,
+            50.00f,
+            60.00f,
+            70.00f,
+            80.00f,
+            9999.00f
+        };
+
+        for (int i = 0; i < cultivation_realm_names_list.Count(); i++)
         {
-            case 10:
-                //level up
-            break;
+            cultivation_realm_dictionary.Add(cultivation_realm_names_list[i], cultivation_realm_exp_threshold_list[i]);
+        };
+    }
+
+    public int CultivationRealmUpdate(float lvl, int realm) //called in ActionsPerTick() to see if the next realm has been reached
+    {
+        if (lvl > cultivation_realm_dictionary.ElementAt(realm).Value)
+        {
+            cultivation_realm_text.text = string.Format("Cultivation Realm:\n{0}", cultivation_realm_dictionary.ElementAt(realm+1).Key);
+            realm = realm + 1;
         }
-        //return realm;
+
+        return realm;
     }
 }
