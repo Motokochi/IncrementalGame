8f2463c271fb25f6ec17f6b1dd1db907de2d4325
Daniel Medina Medina <motokojunior@gmail.com>
2021-06-29 20:05:18 +0100

    Tests with Revi
    Random tests

--- Assets/Scripts/IncrementalGameManager.cs
+++ Assets/Scripts/IncrementalGameManager.cs
@@ -9,6 +9,12 @@ public class IncrementalGameManager : MonoBehaviour
     public float cultivation_lvl;
     public float martial_dao_knowledge = 1;
     public float saint_qi_natural_gain = 0.01f;
+    [Header("Hello")]
+    [SerializeField] private Text cultivation_text;
+    [SerializeField] private float cultivation_lvl;
+    [SerializeField] private float martial_dao_knowledge = 1;
+    [SerializeField] [Range(0.01f, 1.00f)] private float saint_qi_natural_gain = 0.01f;
+    
 
 
     void Start()
@@ -21,6 +27,7 @@ public class IncrementalGameManager : MonoBehaviour
     {
         cultivationText.text = string.Format("Cultivation Level: {0:0.00}" , cultivation_lvl);
         
+        cultivation_text.text = string.Format("Cultivation Level: {0:0.00}" , cultivation_lvl);        
     }
 
 
